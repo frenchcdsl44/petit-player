f7171ece1e393e7d0cfe4022cb1c97bfbdd23a1d
frenchcdsl44 <chcdsl@yahoo.fr>
2022-04-12 00:05:26 +0200

    removed coverage

--- vite.config.ts
+++ vite.config.ts
@@ -13,18 +13,10 @@ export default defineConfig({
 	build:{
 		minify: true,
 		emptyOutDir:false,
-		lib:
-		//[
-			{
-				entry: path.resolve(__dirname, 'src/Petit.js'),
-				name: 'Petit',
-				fileName: (format) => `Petit.${format}.js`
-			},
-			/*{
-				entry: path.resolve(__dirname, 'src/PetitPlayer.js'),
-				name: 'PetitPlayer',
-				fileName: (format) => `PetitPlayer.${format}.min.js`
-			}*/
-		//]
+		lib:{
+			entry: path.resolve(__dirname, 'src/PetitPlayer.js'),
+			name: 'PetitPlayer',
+			fileName: (format) => `PetitPlayer.${format}.min.js`
+		}
 	}
 })
